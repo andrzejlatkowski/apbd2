b39f8eefa8436f799000dc04126d667064fa4835
Andrzej Latkowski <s26442@pjwstk.edu.pl>
2024-04-12 17:40:37 +0200

    added name, email, age validation methods

--- zadanie_solution/LegacyApp/UserService.cs
+++ zadanie_solution/LegacyApp/UserService.cs
@@ -6,24 +6,7 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            // logika biznesowa - walidacja
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
-                return false;
-            }
-
-            // logika biznesowa - walidacja maila
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-            
-            // logika biznesowa 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
+            if (!IsValidName(firstName, lastName) || !IsValidEmail(email) || !IsOldEnough(dateOfBirth))
             {
                 return false;
             }
@@ -74,6 +57,26 @@ namespace LegacyApp
             // infrastruktura - zapis do bazy danych
             UserDataAccess.AddUser(user);
             return true;
+        }
+        private bool IsValidName(string firstName, string lastName)
+        {
+            return !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        private bool IsOldEnough(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+            {
+                age--;
+            }
+            return age >= 21;
         }
     }
 }
